9953903f60d67ae553cb73b6a9f9178476e8c7e4
Kamil Sałęga <kamili.salega@pcgacademia.pl>
2019-10-24 00:20:31 +0200

    KER-015 - Cz.5
    Filtering Data

--- Client/src/app/events/event-details/session-list/session-list.component.ts
+++ Client/src/app/events/event-details/session-list/session-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, OnChanges} from '@angular/core';
 import {ISession} from "../../shared/index";
 
 @Component({
@@ -6,14 +6,33 @@ import {ISession} from "../../shared/index";
     templateUrl: './session-list.component.html',
     styleUrls: ['./session-list.component.css']
 })
-export class SessionListComponent implements OnInit {
+export class SessionListComponent implements OnInit, OnChanges {
 
-    @Input() sessions: ISession[]
+    @Input() sessions: ISession[];
+    @Input() filterBy: string;
+    visibleSessions: ISession[] = [];
 
     constructor() {
     }
 
     ngOnInit() {
+    }
+
+    ngOnChanges(): void {
+
+        if (this.sessions) {
+            this.filterSessions(this.filterBy);
+        }
+    }
+
+    filterSessions(filter) {
+        if (filter === 'all') {
+            this.visibleSessions = this.sessions.slice(0);
+        } else {
+            this.visibleSessions = this.sessions.filter(session => {
+                return session.level.toLocaleLowerCase() === filter;
+            })
+        }
     }
 
 }
