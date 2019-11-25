b2e2037feebb70c7c03c8ac5728d473df5e1dd14
Kamil Sałęga <kamili.salega@pcgacademia.pl>
2019-11-26 00:52:26 +0100

    KER-019 - Cz.5
    Working with Array Keys and Composite Keys

--- Api/Library.Api/Library.Api/Controllers/AuthorCollectionsController.cs
+++ Api/Library.Api/Library.Api/Controllers/AuthorCollectionsController.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using AutoMapper;
+using Library.Api.Helpers;
 using Library.Api.Models;
 using Library.Api.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -22,23 +24,49 @@ namespace Library.Api.Controllers
         }
 
         [HttpPost]
-        public ActionResult<IEnumerable<AuthorDto>> CreateAuthorCollection(IEnumerable<AuthorForCreationDto> authorCollection)
+        public ActionResult<IEnumerable<AuthorDto>> CreateAuthorCollection(
+            IEnumerable<AuthorForCreationDto> authorCollection)
         {
             if (authorCollection == null)
             {
                 return BadRequest();
             }
+
             var authorEntities = _mapper.Map<IEnumerable<Entities.Author>>(authorCollection);
 
             foreach (var author in authorEntities)
             {
                 _libraryRepository.AddAuthor(author);
             }
+
             if (!_libraryRepository.Save())
                 throw new Exception("Creating an author failed on save");
-            
-            return Ok();
+
+            var authorCollectionToReturn = _mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+            var idsAsString = string.Join(",", authorCollectionToReturn.Select(a => a.Id));
+
+            return CreatedAtRoute("GetAuthorCollection", new {ids = idsAsString}, authorCollectionToReturn);
         }
 
+        [HttpGet("({ids})", Name = "GetAuthorCollection")]
+        public ActionResult<IEnumerable<Guid>> GetAuthorCollection([FromRoute] 
+           [ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<Guid> ids)
+        {
+            if (ids == null)
+            {
+                BadRequest();
+            }
+
+            var authorEntities = _libraryRepository.GetAuthors(ids);
+
+            if (ids.Count() != authorEntities.Count())
+            {
+                return NotFound();
+            }
+
+            var authorsToReturn = _mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+
+            return Ok(authorsToReturn);
+        }
     }
 }
