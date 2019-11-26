3c0c728c3047e34feac7dc5b013c6ec5be3885d8
Kamil Sałęga <kamili.salega@pcgacademia.pl>
2019-11-27 00:07:26 +0100

    KER-022 - Cz.1
    Validation and the ApiController Attribute

--- Api/Library.Api/Library.Api/Models/BookForCreationDto.cs
+++ Api/Library.Api/Library.Api/Models/BookForCreationDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Library.Api.Models
 {
     public class BookForCreationDto
     {
+        [Required] 
+        [MaxLength(100)] 
         public string Title { get; set; }
+        
+        [MaxLength(1500)] 
         public string Description { get; set; }
     }
 }
