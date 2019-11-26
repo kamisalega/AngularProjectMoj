bde57f13b2e5ca8c6bab8594b96e471770b0bee9
Kamil Sałęga <kamili.salega@pcgacademia.pl>
2019-11-27 00:28:15 +0100

    KER-022 - Cz.2
    Class-level Input Validation with IValidatableObject

--- Api/Library.Api/Library.Api/Models/BookForCreationDto.cs
+++ Api/Library.Api/Library.Api/Models/BookForCreationDto.cs
@@ -1,8 +1,9 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace Library.Api.Models
 {
-    public class BookForCreationDto
+    public class BookForCreationDto : IValidatableObject
     {
         [Required] 
         [MaxLength(100)] 
@@ -10,5 +11,15 @@ namespace Library.Api.Models
         
         [MaxLength(1500)] 
         public string Description { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Title == Description)
+            {
+                yield  return new ValidationResult(
+                    "The provided description should be different from the title.", new []{"BooksForCreationDto"}
+                    );
+            }
+        }
     }
 }
