0c86bdca2925402d35ffc73415029f29545c51ac
Kamil Sałęga <kamili.salega@pcgacademia.pl>
2019-11-28 23:34:48 +0100

    KER-017 - Cz.1
    Implementing the Session Search

--- Client/src/app/nav/nav-bar.component.ts
+++ Client/src/app/nav/nav-bar.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core'
+import {Component} from '@angular/core'
 import {AuthService} from "../user/auth.service";
+import {EventService, ISession} from "../events/shared";
 
 @Component({
-  selector: 'nav-bar',
-  templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+    selector: 'nav-bar',
+    templateUrl: './nav-bar.component.html',
+    styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent {
-  
-  constructor(private authService: AuthService){
-    
-  }
+
+    searchTerm: string = "";
+    foundSessions: ISession[];
+
+    constructor(private authService: AuthService, private eventService: EventService) {
+
+    }
+
+    searchSessions(searchTerm){
+        this.eventService.searchSessions(searchTerm).subscribe(sessions => {this.foundSessions = sessions});
+    }
 
 }
