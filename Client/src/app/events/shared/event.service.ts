6aa4e54bf85cbe95dfd8f90dbe98dd35945c8af4
Kamil Sałęga <kamili.salega@pcgacademia.pl>
2020-03-14 23:35:51 +0100

    KER-025 - Cz.5
    Using QueryString Parameters

--- Client/src/app/events/shared/event.service.ts
+++ Client/src/app/events/shared/event.service.ts
@@ -28,25 +28,9 @@ export class EventService {
     }
     
 
-    searchSessions(searchTerm: string) {
-        var term = searchTerm.toLocaleLowerCase();
-        var results: ISession[] = [];
-
-        EVENTS.forEach(event => {
-            var matchingSessions = event.sessions.filter(session => session.name.toLocaleLowerCase().indexOf(term) > -1);
-            matchingSessions = matchingSessions.map((session: any) => {
-                session.eventId = event.id;
-                return session;
-            });
-
-            results = results.concat(matchingSessions);
-        });
-
-        var emitter = new EventEmitter(true);
-        setTimeout(() => {
-            emitter.emit(results)
-        }, 100);
-        return emitter;
+    searchSessions(searchTerm: string): Observable<ISession[]>{
+        return this.http.get<ISession[]>('/api/sessions/search?search=' + searchTerm)
+            .pipe(catchError(this.handleError<ISession[]>('getEvent')))
     }
 
     private handleError<T>(operation = 'operation', result?: T) {
