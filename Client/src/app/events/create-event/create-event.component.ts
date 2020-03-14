40a19a61ecc7f4c8f7b2d3d9ae28163faba976dd
Kamil Sałęga <kamili.salega@pcgacademia.pl>
2020-03-14 23:19:39 +0100

    KER-025 - Cz.4
    Using POST and PUT

--- Client/src/app/events/create-event/create-event.component.ts
+++ Client/src/app/events/create-event/create-event.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
-import {EventService} from "../shared";
+import {EventService, IEvent} from "../shared";
 
 @Component({
     selector: 'app-create-event',
@@ -9,20 +9,22 @@ import {EventService} from "../shared";
 })
 export class CreateEventComponent implements OnInit {
 
-    event: any
-    isDirty: boolean = true
+    newEvent: any;
+    isDirty: boolean = true;
 
     constructor(private router: Router, private eventService: EventService) {
+        
     }
 
     ngOnInit() {
     }
 
     saveEvent(formValues) {
-
-        this.eventService.saveEvent(formValues)
-        this.isDirty = false
-        this.router.navigate(['/events'])
+        this.eventService.saveEvent(formValues).subscribe(() => {
+            this.isDirty = false;
+            this.router.navigate(['/events']);
+        });
+        
     }
 
     cancel() {
