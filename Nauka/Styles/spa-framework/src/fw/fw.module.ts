58909f1747bc649dcb37da67e25943fbc3ce8853
Kamil Sałęga <kamili.salega@pcgacademia.pl>
2020-03-29 01:08:01 +0100

    KER-18 - Create the TitleBar Component

---
+++ Nauka/Styles/spa-framework/src/fw/fw.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FrameworkBodyComponent} from "./framework-body/framework-body.component";
+import {ContentComponent} from "./content/content.component";
+import {TitleBarComponent} from "./title-bar/title-bar.component";
+
+
+@NgModule({
+  declarations: [
+    FrameworkBodyComponent,
+    ContentComponent,
+    TitleBarComponent
+  ],
+  exports: [
+    FrameworkBodyComponent,
+    ContentComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FwModule {
+}
